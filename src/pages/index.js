f90044acd91638117728fc1b9e17d4a2513f2162
yuxuan <livelwh@outlook.com>
2025-11-05 17:17:05 +0800

    modfiy t113x docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='docs/T113x/s3-Pro/BoardIntroduction' />;
+  return <Redirect to='docs/T113x/intro' />;
 }
